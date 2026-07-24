e2c92a7c2c1ea07617c3d5aaab3fd2f3616fc103
Akaaljot Singh Mathoda <144289668+Jassa47@users.noreply.github.com>
2026-07-23 21:46:37 -0700

    Add: find-minimum-in-rotated-sorted-array - submission-1

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-1.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun findMin(nums: IntArray): Int {
+        var l = 0
+        var r = nums.size - 1
+        while (l < r) {
+            val m = (r+l) / 2
+            if (nums[m] < nums[r]) {
+                r = m
+            } else {
+                l = m + 1
+            }
+        }
+        return nums[l]
+    }
+}
