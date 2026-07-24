19d864198a8eb4d42d262c6ba04332c121ee906a
Akaaljot Singh Mathoda <144289668+Jassa47@users.noreply.github.com>
2026-07-23 22:54:31 -0700

    Add: sort-colors - submission-4

---
+++ Data Structures & Algorithms/sort-colors/submission-4.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun sortColors(nums: IntArray) {
+        var count = IntArray(3)
+
+        for(num in nums)
+        {
+           count[num]++
+        }
+
+        var index = 0
+       for(i in 0 until 3)
+       {
+        while(count[i]!=0)
+        {
+            nums[index++] = i
+            count[i]=count[i]-1
+        }
+       }
+       
+
+    }
+}
