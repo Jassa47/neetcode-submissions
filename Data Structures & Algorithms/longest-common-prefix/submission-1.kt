eafdb7919b632be2955300872f874a46498c9387
Akaaljot Singh Mathoda <144289668+Jassa47@users.noreply.github.com>
2026-07-23 22:13:19 -0700

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun longestCommonPrefix(strs: Array<String>): String {
+        for(i in strs[0].indices)
+        {
+            for( s in strs)
+            {
+                if(i == s.length || s[i] != strs[0][i])
+                {
+                    return s.substring(0,i)
+                }
+            }
+
+        }
+        return strs[0]
+    }
+}
